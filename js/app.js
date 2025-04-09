db9da7914dac1a278091e56cfc4616cbca914e09
azizbek <azizbek>
2025-04-09 14:00:35 +0500

    add

--- js/app.js
+++ js/app.js
@@ -1,6 +1,7 @@
-//===============
 let number = prompt("son kiriting: ");
 if (number >= 0 && number <= 999999999999999999999999999999999999999999999n) {
+  console.log(alert("bu son"));
+} else if (number <= 0) {
   console.log(alert("bu son"));
 } else {
   console.log(alert("bu son emas"));
